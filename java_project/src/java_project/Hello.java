9c1b3088a666e3e7d6d723fe4ffb30883224ea23
prabhat-arya <parya7799@gmail.com>
2020-05-24 18:50:55 +0530

    commit2

--- java_project/src/java_project/Hello.java
+++ java_project/src/java_project/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("this is java program ");
 		System.out.println("this is from github changes");
+		System.out.println("without pulling the new code we can't commit our code");
 	}
 }
