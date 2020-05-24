5db4f023691397cef9bf6a8b66c8b7b6ed634cdc
prabhat-arya <62637345+prabhat-arya@users.noreply.github.com>
2020-05-24 18:49:17 +0530

    Update Hello.java
    one more print message

--- java_project/src/java_project/Hello.java
+++ java_project/src/java_project/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("this is java program ");
 		System.out.println("this is from github changes");
+		System.out.println("before going to write the code first we have to pull the code  ");
+		
 	}
 }
