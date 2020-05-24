03fd231c79e5f6ead87d6c48bc4b33e6f21f8d56
prabhat-arya <62637345+prabhat-arya@users.noreply.github.com>
2020-05-24 18:38:16 +0530

    Update Hello.java
    commit from github

--- java_project/src/java_project/Hello.java
+++ java_project/src/java_project/Hello.java
@@ -3,5 +3,6 @@ package java_project;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("this is java program ");
+		System.out.println("this is from github changes");
 	}
 }
